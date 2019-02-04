58146808cb2436ba7b84e3989359ae4a1df44ac6
Mitko <mito_vr@abv.bg>
2019-02-05 00:15:07 +0200

    app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var path = require('path');
 
 const envConfig=require('./config/env-config')
 const initDb=require('./config/db-config')
-
+const initMidleWare =require('./config/middleware')
 const enviroment = process.env.NODE_ENV || 'development'
 
 
@@ -12,6 +12,8 @@ var app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
+
+initMidleWare(app)
 
 app.use(express.static(path.join(__dirname, 'public')));
 initDb(envConfig[enviroment].connectionString)
